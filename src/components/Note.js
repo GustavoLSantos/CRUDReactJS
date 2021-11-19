7ca9887725db230d62ae00f7c2ca95ee287ca2e3
Gustavo Lira <gustavolirasn@gmail.com>
2021-11-19 17:14:55 -0300

    checkboxes

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,39 +1,53 @@
-
-import { MdDeleteForever } from 'react-icons/md';
 import React from 'react';
 import { useState } from "react";
 import Modal from "./Modal";          
-import { BsFillBrushFill } from 'react-icons/bs';
+import Checkbox from '@material-ui/core/Checkbox';
 
-const Note = ({ handleUpdateClick, id, type, text, date, handleDeleteNote }) => {
+
+const Note = ({ handleUpdateClick, id, title, type, text, date, handleDeleteNote }) => {
 
     const [isModalVisible, setIsModalVisible] = useState(false);
     const [noteText,setNoteText] = useState('');
+    const [checked, setChecked] = useState(false);
     const handleChange = (event) => {
         setNoteText(event.target.value);
     }   
-   
+
+
+    const toggleCheckbox = event => {
+        setChecked(event.target.checked);
+    }
 
     return (
         <div className={type}>
             <div className="note-header">
-                <BsFillBrushFill onClick={() => setIsModalVisible(true)}/>
-                <MdDeleteForever onClick={() => handleDeleteNote(id)} className='delete-icon' size='1.3em' />
+                <Checkbox checked={checked} color="default" value={text} onChange={toggleCheckbox}/>
+                <span className="title-header">{title}</span>
+                <span class="material-icons" onClick={() => setIsModalVisible(true)}>edit</span>
+                <span class="material-icons" onClick={() => handleDeleteNote(id)}>delete</span>
             </div>
-
-            <span>{text}</span>
-            <div className="note-footer">
-                <small>{date}</small>
-                {isModalVisible ? <Modal onClose={() => setIsModalVisible(false)}>
-                <div className="note new">
+            {isModalVisible ? <Modal onClose={() => setIsModalVisible(false)}>
+            <div className="update-container">
+                <div className="header-noteupdate">
+                <textarea rows="1" cols="10" placeholder={title} className="title-field" disabled></textarea>
+                <select className="select-newnote" disabled>
+                    <option value="notype">{type}</option>
+                </select>
+                </div>
+                <div className="description-field-update">
                     <textarea className="update-note" rows="8" cols="10" placeholder={text} onChange={handleChange} value={noteText}>
                     </textarea>
                     <div className="note-footer">
-                        <button className="save" onClick={() => handleUpdateClick(id, noteText)}>UPDATE</button>
                         <button className="cancel" onClick={() => setIsModalVisible(false)}>CANCEL</button>
+                        <button className="save" onClick={() => handleUpdateClick(id, noteText)}>UPDATE</button>
                     </div>
                 </div>
-                </Modal> : null}
+            </div>
+            
+            </Modal> : null}
+            <span>{checked ? <strike>{text}</strike> : text}</span>
+            <div className="note-footer">
+                <small>{date}</small>
             </div>
         </div>
     )
